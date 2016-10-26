b2946d91e6da8548c834735cfbf96a0e7716cd53
yura <yura@com.ua>
2016-10-26 20:21:29 +0300

    star page of site and nd to gitignor list

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Yo=ho-ho</h1>
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT_qaljo-cQdTdrmJVgQbIidouAd9XuUX0WYT-p725hv_fZh7gi" style="width: 100%"/>
+    </body>
+</html>
